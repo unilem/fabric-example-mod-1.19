3039e3d1384bbd1300af70fce7f7459862c5aee9
unilem <ylemlong@outlook.com>
2023-02-18 01:17:16 +0800

    add two items

--- src/main/java/net/ylem/tutorialmod/TutorialMod.java
+++ src/main/java/net/ylem/tutorialmod/TutorialMod.java
@@ -1,12 +1,20 @@
 package net.ylem.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Item RAW_TANZANITE = Registry.register(Registries.ITEM, new Identifier(MOD_ID, "raw_tanzanite"), new Item(new FabricItemSettings()));
+	public static final Item TANZANITE = Registry.register(Registries.ITEM, new Identifier(MOD_ID, "tanzanite"), new Item(new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
